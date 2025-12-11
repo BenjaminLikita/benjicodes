969788f50e233a0acf0dda7bba0b7272da3adb95
Benjamin Likita <benjaminlikita3@gmail.com>
2025-12-11 01:04:40 +0100

    updated footer component to display the current year dynamically

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,10 @@
 
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="grid place-items-center bg-black-1 py-3 text-sm text-white">
-        <span>&copy; 2024 Likita Benjamin - All rights reserved</span>
+        <span>&copy; {year} Likita Benjamin - All rights reserved</span>
     </footer>
   )
 }
